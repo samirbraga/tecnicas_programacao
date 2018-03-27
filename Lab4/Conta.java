65db349ec2dfbb54320f76e34e9281a5c2218ccf
samirbraga <samirchavess@gmail.com>
2018-03-27 08:56:19 -0300

    Sistema Adicionado

---
+++ Lab4/Conta.java
@@ -0,0 +1,5 @@
+abstract class Conta {
+	int id;
+	double saldo;
+	String tipo;
+}
---
+++ Lab4/Sistema.java
@@ -0,0 +1,28 @@
+class Sistema {
+	static int n;
+	Conta[] contas = new Conta[100];
+
+	Conta criarContaXPTOBasic () {
+		int id = n;
+		ContaBasic basic = new ContaBasic(id);
+		contas[n] = basic;
+		n++;
+		return basic;
+	}
+
+	Conta criarContaXPTOPlus () {
+		int id = n;
+		ContaPlus plus = new ContaPlus(id);
+		contas[n] = plus;
+		n++;
+		return plus;
+	}
+
+	Conta criarContaXPTOExtreme () {
+		int id = n;
+		ContaExtreme extreme = new ContaExtreme(id);
+		contas[n] = extreme;
+		n++;
+		return extreme;
+	}
+}
