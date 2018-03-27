9cc27e3c8b68e36b26b7ac0c3d160a96b0cf0910
dqrtec <dqrtec@hotmail.com>
2018-03-27 09:25:02 -0300

    plus

--- Lab4/Plus.java
+++ Lab4/Plus.java
@@ -1,3 +1,16 @@
 class Plus extends Conta {
+	public boolean credito(double credito){ //sobrecarga
+		this.saldo = this.saldo + (saldo*1.005);
+		return true;
+	}
 
+	public boolean debito(double debito){ //sobrecarga
+		resultadoDebito = this.saldo - debito;
+		if (resultadoDebito <= 0) {
+			return false;
+		}
+		this.saldo = resultadoDebito;
+		return true;
+
+	}
 }
